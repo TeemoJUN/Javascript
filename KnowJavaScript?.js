9ecd1496a9259699bd877dba13d86f72e1018030
Teemo <18720949926@163.com>
2017-01-25 19:36:56 +0800

    Update KnowJavaScript?.js

--- KnowJavaScript?.js
+++ KnowJavaScript?.js
@@ -3,6 +3,7 @@
 * 这里收集一些好玩的js
 *很多人会问为什么这样写，但这样有结果！说明必有原因
 */
+//........0.........
 //................................................................................................................
   var bar="Lin";
   var Obj={
@@ -19,3 +20,44 @@
 //相当于var c=Obj.foo;c();即在先是引用，在执行！！
 //.................平常写的时候注意加....this......
 //................................................................................................................
+
+//.......1.......
+<input type="file" id="a" onchange="foo(event,this)" multiple="multiple">
+  <script type="text/javascript">
+     function foo(event,that){
+         console.log(arguments);
+                                /*
+                                [change , input#a 属性(property)值 = "5.png" 属性(attribute)值 = "null"]
+                                0
+                                  change 
+
+                                1
+                                  input#a 属性(property)值 = "5.png" 属性(attribute)值 = "null"
+
+                                __proto__
+                                  []
+                                */
+         console.log(event);// change 
+         console.log(that);// <input id="a" type="file" multiple="multiple" onchange="f(event,this)">
+         console.log(event.target)// <input id="a" type="file" multiple="multiple" onchange="f(event,this)">
+         console.log(event.target===a);//true
+         console.log(that.value);//5.png
+         console.log(that.files);// FileList { 0=File,  length=1,  item=item()}
+      }
+  </script>
+//由此可以知道event事件是event.target就是this
+//.....注意.....event是change！
+//.........事件委托的原因.............
+
+
+
+
+
+
+
+
+
+
+
+
+
