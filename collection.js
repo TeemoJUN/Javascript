305364b9d559abd82da7eb4262b71c0734406677
Teemo <18720949926@163.com>
2017-04-04 21:22:34 +0800

    Update collection.js

--- collection.js
+++ collection.js
@@ -97,12 +97,20 @@ function log() {
   }
 }
 
+//javascript 实现重载
+ function addMethod(object,name,fn){
+    var old=object[name];
+    object[name]=function(){
+        if(fn.length==arguments.length){
+            return fn.apply(this,arguments);
+        }
+        else if(typeof old=='function'){
+            return old.apply(this,arguments);
+        }
+     }
+ }
 
-
-
-
-
-
+       
 
 
 
